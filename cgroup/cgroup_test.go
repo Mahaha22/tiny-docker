0e6fdc037d8a48f67357c785442a9e152f2ca845
haohao <mahaohao1999@163.com>
2023-05-22 11:40:37 +0800

    cgroup功能测试

--- cgroup/cgroup_test.go
+++ cgroup/cgroup_test.go
@@ -9,8 +9,20 @@ func TestCpu(t *testing.T) {
 	Cpu := CpuSubsystem{}
 	res := conf.Cgroupflag{
 		Cpulimit: "10000",
+		Memlimit: "100m",
 	}
 	Cpu.Init("container", res)
-	Cpu.Apply("container", 1000)
+	Cpu.Apply("container", 38406)
 	Cpu.Delete("container")
+}
+
+func TestMem(t *testing.T) {
+	Mem := MemSubsystem{}
+	res := conf.Cgroupflag{
+		Cpulimit: "10000",
+		Memlimit: "100m",
+	}
+	Mem.Init("container", res)
+	Mem.Apply("container", 40068)
+	Mem.Delete("container")
 }
