f5439aa3d004491068abf49717c75c8686e3fe94
haohao <mahaohao1999@163.com>
2023-05-26 00:48:47 +0800

    启动容器所需的库

--- container/container.go
+++ container/container.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"syscall"
 	"tiny-docker/cgroup"
 	"tiny-docker/conf"
@@ -45,28 +46,29 @@ func (c *Container) Init() error {
 	/*
 		待补充
 	*/
-	//cmd := exec.Command("sleep", "30000")
-	cmd := exec.Command("/bin/bash")                     //模拟容器的第一个命令执行
-	cmd.SysProcAttr = c.NameSpaceRes                     //独立命名空间配置
-	stdinPipe, _ := cmd.StdinPipe()                      //创建一个连接子进程输入流的管道用于父进程向内传递信息
-	mountinfo := "mount -t proc proc /proc -o private\n" //挂载proc private标志，表示这个挂载点是与其他挂载点相互独立的，不会影响其他挂载点，也不会被其他挂载点所影响。
-	chrootinfo := "chroot /root/busybox\n"               //设置根目录
-	pathinfo := "export PATH=:/bin\n"                    //设置环境变量
+	os.Chdir("/root/go/tiny-docker/container/lib") //钩子程序的位置
+	cmd := exec.Command("./task")                  //启动容器的钩子
+	r, w, _ := os.Pipe()                           //用于跟这个钩子程序通信
+	cmd.ExtraFiles = []*os.File{w}                 //将管道的一端传递给钩子
 	if err := cmd.Start(); err != nil {
-		return fmt.Errorf("cmd.Start() fail %v", err)
+		fmt.Println("cmd err = ", err)
+		return err
 	}
-	c.RealPid = cmd.Process.Pid
+
+	buf := make([]byte, 1024)
+	n, _ := r.Read(buf)
+	r.Close()
+	pid, _ := strconv.Atoi(string(buf[:n-1]))
+	c.RealPid = pid
+
 	//2.容器内的第一个程序已经启动，针对这个容器以他的唯一标识container_id在/sys/fs/cgroup/<subsystem>/tiny-docker/<container_id>创建新的subsystem
 	if err := cgroup.SetCgroup(c.ContainerId, &c.CgroupRes, c.RealPid); err != nil {
 		return fmt.Errorf("cgroup资源配置出错 :%v", err)
 	}
-
-	stdinPipe.Write([]byte(mountinfo))
-	stdinPipe.Write([]byte(chrootinfo))
-	stdinPipe.Write([]byte(pathinfo))
 	//cmd.Wait()
 	return nil
 }
+
 func (c *Container) Remove() {
 	process, _ := os.FindProcess(c.RealPid)
 	process.Signal(syscall.Signal(9))
---
+++ container/lib/task.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func child() {
+	fmt.Println("[exe]", "pid:", os.Getpid())
+	err := syscall.Sethostname([]byte("mycontainer"))
+	if err != nil {
+		fmt.Println("sethostname err = ", err)
+		return
+	}
+	os.Chdir("/root/busybox")
+	err = syscall.Mount("proc", "proc", "proc", 0, "")
+	if err != nil {
+		fmt.Println("mount err = ", err)
+		return
+	}
+	err = syscall.Chroot(".")
+	if err != nil {
+		fmt.Println("chroot err = ", err)
+		return
+	}
+	cmd := exec.Command("/bin/sh", "-c", "/bin/sleep 3000") //模拟容器中启动一个服务
+	//cmd := exec.Command("/bin/sh")
+	cmd.Env = append(cmd.Env, ":/bin") //添加环境变量
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Start(); err != nil {
+		return
+	}
+	cmd.Wait()
+	syscall.Unmount("proc", 0)
+}
+
+func main() {
+	if os.Args[0] == "/proc/self/exe" {
+		//子进程用于真正的启动一个容器
+		child()
+	} else {
+		//父进程用于创建隔离的命名空间
+		fmt.Println("[main]", "pid:", os.Getpid())
+		cmd := exec.Command("/proc/self/exe")
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Cloneflags: syscall.CLONE_NEWUTS |
+				syscall.CLONE_NEWNET |
+				syscall.CLONE_NEWIPC |
+				syscall.CLONE_NEWUSER |
+				syscall.CLONE_NEWNS |
+				syscall.CLONE_NEWPID,
+			UidMappings: []syscall.SysProcIDMap{
+				{ContainerID: 0, HostID: 0, Size: 1},
+			},
+			GidMappings: []syscall.SysProcIDMap{
+				{ContainerID: 0, HostID: 0, Size: 1},
+			},
+			Unshareflags: syscall.CLONE_NEWNS,
+		}
+		if err := cmd.Start(); err != nil {
+			return
+		}
+		w := os.NewFile(uintptr(3), "pipe")
+		fmt.Fprintln(w, cmd.Process.Pid)
+		w.Close()
+		cmd.Wait()
+	}
+}
