3ecfa9b67f2deec01df35c320ef620d8b25d4170
haohao <mahaohao1999@163.com>
2023-05-31 16:33:56 +0800

    修改err返回细节

--- cmd/newTerm.go
+++ cmd/newTerm.go
@@ -11,21 +11,24 @@ import (
 	"tiny-docker/grpc/term"
 )
 
-func newTerm(containerId string) {
+func newTerm(containerId string) error {
 	fmt.Println("new term")
 	client, err := conn.GrpcClient_Double()
 	if err != nil {
-		log.Fatal("newTerm client error : ", err)
+		return err
+		//log.Fatal("newTerm client error : ", err)
 	}
 	stream, err := client.Newterm(context.Background())
 	if err != nil {
-		log.Fatal("newTerm stream error : ", err)
+		return err
+		//log.Fatal("newTerm stream error : ", err)
 	}
 	//接收服务器的信息
 	go recv(stream)
 	//首先把需要开启交互的容器的id传过去建立交互连接
 	if err := stream.Send(&term.Request{Input: containerId}); err != nil {
-		log.Fatalf("failed to send containerId: %v", err)
+		return err
+		//log.Fatalf("failed to send containerId: %v", err)
 	}
 	//从终端读取用户输入，并将其发送到Shell服务中执行
 	scanner := bufio.NewScanner(os.Stdin)
@@ -36,13 +39,15 @@ func newTerm(containerId string) {
 		}
 		cmd := scanner.Text()
 		if err := stream.Send(&term.Request{Input: cmd}); err != nil {
-			log.Fatalf("failed to send command: %v", err)
+			return err
+			//log.Fatalf("failed to send command: %v", err)
 		}
 		if cmd == "exit" {
 			break
 		}
 	}
 	//time.Sleep(time.Second * 1)
+	return nil
 }
 
 func recv(stream term.Term_NewtermClient) {
