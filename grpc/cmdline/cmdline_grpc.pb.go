30892b0f18c39767e70a144322efa7a708628031
haohao <mahaohao1999@163.com>
2023-05-31 19:59:42 +0800

    增加清除容器rpc调用

--- grpc/cmdline/cmdline_grpc.pb.go
+++ grpc/cmdline/cmdline_grpc.pb.go
@@ -28,6 +28,8 @@ type ServiceClient interface {
 	PsContainer(ctx context.Context, in *Request, opts ...grpc.CallOption) (*ContainerInfo, error)
 	// 给容器发送指令
 	ExecContainer(ctx context.Context, in *Request, opts ...grpc.CallOption) (*ContainerStdout, error)
+	// 杀死/清除容器
+	KillContainer(ctx context.Context, in *Request, opts ...grpc.CallOption) (*RunResponse, error)
 }
 
 type serviceClient struct {
@@ -65,6 +67,15 @@ func (c *serviceClient) ExecContainer(ctx context.Context, in *Request, opts ...
 	return out, nil
 }
 
+func (c *serviceClient) KillContainer(ctx context.Context, in *Request, opts ...grpc.CallOption) (*RunResponse, error) {
+	out := new(RunResponse)
+	err := c.cc.Invoke(ctx, "/cmdline.Service/KillContainer", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ServiceServer is the server API for Service service.
 // All implementations must embed UnimplementedServiceServer
 // for forward compatibility
@@ -75,6 +86,8 @@ type ServiceServer interface {
 	PsContainer(context.Context, *Request) (*ContainerInfo, error)
 	// 给容器发送指令
 	ExecContainer(context.Context, *Request) (*ContainerStdout, error)
+	// 杀死/清除容器
+	KillContainer(context.Context, *Request) (*RunResponse, error)
 	mustEmbedUnimplementedServiceServer()
 }
 
@@ -90,6 +103,9 @@ func (UnimplementedServiceServer) PsContainer(context.Context, *Request) (*Conta
 }
 func (UnimplementedServiceServer) ExecContainer(context.Context, *Request) (*ContainerStdout, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ExecContainer not implemented")
+}
+func (UnimplementedServiceServer) KillContainer(context.Context, *Request) (*RunResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method KillContainer not implemented")
 }
 func (UnimplementedServiceServer) mustEmbedUnimplementedServiceServer() {}
 
@@ -158,6 +174,24 @@ func _Service_ExecContainer_Handler(srv interface{}, ctx context.Context, dec fu
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Service_KillContainer_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Request)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ServiceServer).KillContainer(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/cmdline.Service/KillContainer",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ServiceServer).KillContainer(ctx, req.(*Request))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Service_ServiceDesc is the grpc.ServiceDesc for Service service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -176,6 +210,10 @@ var Service_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "ExecContainer",
 			Handler:    _Service_ExecContainer_Handler,
+		},
+		{
+			MethodName: "KillContainer",
+			Handler:    _Service_KillContainer_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
