f6beed830e2e67086e5dc4c8aad7c14810d8e385
haohao <mahaohao1999@163.com>
2023-06-04 18:03:11 +0800

    不能简单的返回nil，需要返回&emptypb.Empty{},以至于grpc可以成功序列化

--- server/service/createNetwork.go
+++ server/service/createNetwork.go
@@ -12,14 +12,16 @@ import (
 func (r *ContainerService) CreateNetwork(ctx context.Context, req *cmdline.Network) (*emptypb.Empty, error) {
 	_, subnet, _ := net.ParseCIDR(req.Subnet)
 	nw := &network.Network{
-		Name:   req.Name,
-		Subnet: subnet,
-		Driver: network.NewDriver(req.Driver),
+		Name:    req.Name,                       //网络名
+		Subnet:  subnet,                         //子网划分
+		Driver:  network.NewDriver(req.Driver),  //网络驱动
+		Ipalloc: network.NewIPAllocator(subnet), //初始化一个ip分配器，这里面保存所以已分配的ip
 	}
+
 	network.Global_Network[nw.Name] = nw //加入全局map表中
 	err := nw.CreateNetwork()            //根据配置新建网络
 	if err != nil {
-		return nil, err
+		return &emptypb.Empty{}, err
 	}
-	return nil, nil
+	return &emptypb.Empty{}, nil
 }
