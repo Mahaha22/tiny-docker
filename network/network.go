a7b351ce5dc2de849742ab9f3f75c21f8df4b62d
haohao <mahaohao1999@163.com>
2023-06-04 18:02:05 +0800

    网络管理

--- network/network.go
+++ network/network.go
@@ -1,17 +1,28 @@
 package network
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 var Global_Network map[string]*Network //保存全局的网络信息
 func Global_Network_Init() {
 	Global_Network = make(map[string]*Network)
 }
+func RemoveAllNetwork() {
+	for name, nw := range Global_Network {
+		err := nw.RemoveNetwork()
+		if err != nil {
+			fmt.Printf("network %s delete error:%v\n", name, err)
+		}
+	}
+}
 
 type Network struct { //单个新创建的网络信息包含网络名、子网划分、网络驱动
-	Name   string     `json:"name"`   //网络名
-	Subnet *net.IPNet `json:"subnet"` //子网
-	Ips    string     `json:"ips"`    //网络划分
-	Driver Driver     `json:"driver"` //网络驱动
+	Name    string     `json:"name"`    //网络名
+	Subnet  *net.IPNet `json:"subnet"`  //子网
+	Ipalloc *IPAlloc   `json:"ipalloc"` //网络划分
+	Driver  Driver     `json:"driver"`  //网络驱动
 }
 
 func (n *Network) CreateNetwork() error {
@@ -19,4 +30,13 @@ func (n *Network) CreateNetwork() error {
 
 	//1.创建网络驱动
 	return n.Driver.Create(n)
+}
+
+func (n *Network) RemoveNetwork() error {
+	err := n.Driver.Remove() //删除驱动
+	if err != nil {
+		return err
+	}
+	delete(Global_Network, n.Name) //从network网络map表中删除
+	return nil
 }
