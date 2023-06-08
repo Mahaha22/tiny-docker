ba631d805b90c1ac31e4828113192e3869ec3a51
haohao <mahaohao1999@163.com>
2023-06-08 21:22:13 +0800

    小改动

--- server/service/term/newTerm.go
+++ server/service/term/newTerm.go
@@ -45,8 +45,19 @@ func (t *TermService) Newterm(stream term.Term_NewtermServer) error {
 		// var wg sync.WaitGroup
 		// wg.Add(3)
 		//chrootinfo := "chroot /root/busybox /bin/sh\n" //设置容器根目录
+		// root := " /mnt/tiny-docker/" + container.ContainerId + "/merge "
+		// chrootinfo := "chroot" + root + "||" + " chroot" + root + "/bin/sh \n"
+		/*
+			解释一下这个chrootinfo的含义,给个例子chroot path  > error.txt 2>&1 && rm -rf error.txt || chroot path /bin/sh
+			chroot path用于改变当前程序的根文件路径默认启动/bin/bash，然而很多容器不一定有bash这个命令所以就有可能报错
+			chroot .
+			chroot: failed to run command ‘/bin/bash’: No such file or directory
+			因此这个命令的含义就是先执行chroot path;如果有报错就把报错重定向到一个文件然后再删除这个文件，目的就是不显示报错信息
+			当前一个命令执行失败就执行后一个命令chroot path /bin/sh。大多数容器都有sh
+		*/
+		//fmt.Println(chrootinfo)
 		chrootinfo := "chroot /mnt/tiny-docker/" + container.ContainerId + "/merge /bin/sh\n"
-		pathinfo := "export PATH=:/bin\n" //设置环境变量
+		pathinfo := "export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/bin:/sbin \n" //设置环境变量
 		stdinPipe.Write([]byte(chrootinfo))
 		stdinPipe.Write([]byte(pathinfo))
 
@@ -55,8 +66,10 @@ func (t *TermService) Newterm(stream term.Term_NewtermServer) error {
 		go errProcess(stderrPipe, stream)  //错误流
 		//wg.Wait()
 		//新创建的终端需要加入容器的cgroup以受到资源的限制
-		cgroup.ApplyCgroup(container.ContainerId, cmd.Process.Pid)
-
+		err := cgroup.ApplyCgroup(container.ContainerId, cmd.Process.Pid)
+		if err != nil {
+			fmt.Println("ApplyCgroup fail = ", err)
+		}
 		//在终端内部可以通过exit主动退出，但是当客户端意外退出时，也应具备关闭远程终端的能力，避免浪费资源
 		go Killcmd(cmd.Process.Pid, stdinPipe)
 		cmd.Wait()
